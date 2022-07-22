22b1cdfe1125ee9978e0af47fedb4c37581f8920
Mario Rawady <rawady_mario@hotmail.com>
2022-07-22 10:48:28 +0300

    AntiHack

--- tests/PhpUnits/Helpers/AntiHackTest.php
+++ tests/PhpUnits/Helpers/AntiHackTest.php
@@ -4,6 +4,7 @@
 	//To Run: .\vendor/bin/phpunit .\tests\PhpUnits\Helpers\AntiHackTest.php
 	use PHPUnit\Framework\TestCase;
 	use RawadyMario\Helpers\AntiHack;
+use RawadyMario\Helpers\Helper;
 
 	final class AntiHackTest extends TestCase {
 
@@ -60,6 +61,30 @@
 						"category" => "Software Engineer",
 					]
 				]
+			];
+
+			$this->assertEqualsCanonicalizing($expected, $_GET);
+		}
+
+		public function testCheckSimpleArrayWithCustomKeySuccess() {
+			$_GET = [
+				"key" => "SELECT * FROM `users` WHERE `email` = 'test@hotmail.com' AND `active` = 1 AND `verified` = 1",
+				"limit" => '100',
+				"offset" => '50',
+				"page" => '1',
+				"category" => "Software Engineer"
+			];
+
+			$_GET = AntiHack::Check($_GET, [
+				'query'
+			]);
+
+			$expected = [
+				"key" => Helper::TruncateStr("_select_ * _from_ `users` _where_ `email` = 'test@hotmail.com' and `active` = 1 and `verified` = 1", 50 + 6, ""), //+ 6: Number of underscores added
+				"limit" => '100',
+				"offset" => '50',
+				"page" => '1',
+				"category" => "Software Engineer"
 			];
 
 			$this->assertEqualsCanonicalizing($expected, $_GET);
